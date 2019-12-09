cda8b2571230618e90138e0a0b99666fb1e5dfea
Jacob Harrington <jharrington4@gmail.com>
2019-12-09 15:45:36 -0800

    created / GET route and test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-
+  it('should a 404 given a no request', async() => {
+    return await request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('hi');
+      });
+  });
 });
