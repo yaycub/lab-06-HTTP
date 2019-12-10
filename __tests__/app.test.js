4f45e6188f47024416688c9f6a21bb5a9a36f06c
Jacob Harrington <jharrington4@gmail.com>
2019-12-09 16:07:36 -0800

    finished POST /echo route and test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,11 +2,20 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-  it('should a 404 given a no request', async() => {
+  it('should a return hi given a GET request at /', async() => {
     return await request(app)
       .get('/')
       .then(res => {
-        expect(res.text).toEqual('hi');
+        expect(res.text).toEqual('<h1>hi</h1>');
+      });
+  });
+
+  it('should echo what the body of a POST request', async() => {
+    return await request(app)
+      .post('/echo')
+      .send('poop scoot')
+      .then(res => {
+        expect(res.text).toEqual('poop scoot');
       });
   });
 });
